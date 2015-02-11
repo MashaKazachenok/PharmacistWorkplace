613685a547ce240128f053d6972185bd6c5a4ffc
m.kazachenok <kazachenokm@mail.ru>
2015-02-11 21:55:43 +0300

    добавила контроллер ClientsController.cs

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -8,11 +8,6 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
