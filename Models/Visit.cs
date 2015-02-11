e2788f96f76eabcd0b972f55a96192186714a7dd
m.kazachenok <kazachenokm@mail.ru>
2015-02-11 22:24:48 +0300

    добавила модели предметной области

---
+++ Models/Visit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Visit
+    {
+        public DateTime VisitData { get; set; }
+        public int OrderAmount { get; set; }
+        public string OrderStatus { get; set; }
+        public Client Owner { get; set; }
+    }
+}
