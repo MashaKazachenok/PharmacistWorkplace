f55f9dc671574fa122489d5fa8b5ceba988a8788
m.kazachenok <kazachenokm@mail.ru>
2015-02-12 20:46:50 +0300

    Добавила ClientRepository.cs

---
+++ Repositories/ClientRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+
+namespace Repositories
+{
+    public class ClientRepository
+    {
+        public int CreateClient(Client client)
+        {
+            return 0;
+        }
+
+        public int EditClient(Client client)
+        {
+            return 0;
+        }
+
+        public List<Client> GetAll()
+        {
+            return new List<Client>();
+        }
+
+        public Client GetById(int id)
+        {
+            return new Client();
+        }
+    }
+}
