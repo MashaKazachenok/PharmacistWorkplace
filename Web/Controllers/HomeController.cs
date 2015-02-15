c9b0b10c1f3fcb8696d95aa05380e8eb2e438144
m.kazachenok <kazachenokm@mail.ru>
2015-02-15 21:46:05 +0300

    добавила модальные окна для удаления визитов и клиентов

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -8,13 +8,6 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
